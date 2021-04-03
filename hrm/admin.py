8e962a1e469cbbb30e9ec72e4d2240dff9822ed1
gembatplc <gembatplc@github.com>
2021-04-03 18:31:21 +0600

    work with model and admin site

--- hrm/admin.py
+++ hrm/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Department,Division
 # Register your models here.
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('__str__','name','description')
+    list_filter = ('name',)
+    list_editable = ('name',)
+    list_per_page = 10
+admin.site.register(Department)
+
+class DivisionAdmi(admin.ModelAdmin):
+    list_display = ('name','department','description')
+    list_filter = ('name','department')
+    list_editable = ('name','department')
+    list_per_page = 10
+admin.site.register(Division)
--- hrm/models.py
+++ hrm/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True,null=True)
+    def __str__(self):
+        return ('name','desription')
+
+class Division(models.Model):
+    department = models.ForeignKey(Department,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True,null=True)    
